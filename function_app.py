f7ab1eb58c3af5ff2476a13384eb9b8a5ef0c2ef
Satyajeet <satyajeetk@hexaware.com>
2024-05-16 08:12:54 +0000

    time changed

--- function_app.py
+++ function_app.py
@@ -3,7 +3,7 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 */5 3-12 * * 1-5", arg_name="myTimer", run_on_startup=True,
+@app.schedule(schedule="0 */5 3-10 * * 1-5", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def RSITrigger(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
